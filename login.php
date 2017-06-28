03046d665537458e8a2edbe81e51146bc7a6808d
Xeena93 <alexa93k@gmail.com>
2017-06-28 15:53:05 +0200

    modifiche su login.php e logout.php logout è incompleta. login da controllare

--- login.php
+++ login.php
@@ -7,15 +7,30 @@ mysqli_select_db($conn,$db_database);
 	$username=$_POST["username"];
 	$password=$_POST["password"];
 	//cerca la coppia username password nel database
-	$ricercaUP= "SELECT * FROM utente WHERE username =\"$username\" AND password=\"$password\"";
+	$ricercaUP= "SELECT * FROM utente WHERE username ='".$username."' AND password='".$password."'";
 	$result = mysqli_query($conn, $ricercaUP);
-	if($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){//utente trovato, mando un json con loggato
-	$data = array(
-	"id" => $row["id"],
-	"logged" => $row["logged"]
-	)
-	 $json=json_encode($data);
-     echo $json;
+	//cerchiamo l'id dell'utente
+	// $idName= "SELECT id_usr FROM utente WHERE username =\"$username\""
+	// $result1 = mysqli_query($conn, $idName);
+
+	if($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){//utente trovato,
+		$queryLog= "UPDATE utente SET logged='1' WHERE id_usr ='".$row["id_usr"]."'";
+		$ricercaUP= "SELECT * FROM utente WHERE username ='".$username."' AND password='".$password."'";
+		$result = mysqli_query($conn, $ricercaUP);
+		$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+		//separiamo con il fetch il nostro array in righe e settiamo logged ad 1, si è loggato
+		// if($row2 = mysqli_fetch_array($result1, MYSQLI_ASSOC)){
+		// 	$queryLog= "UPDATE utente SET logged='1' WHERE id_usr =\"$row2\" ";
+	 		//poi mando un json con loggato
+			$data = array(
+				"id" => $row["id_usr"],
+				"logged" => $row["logged"]
+			)
+		 $json=json_encode($data);
+	     echo $json;
+
+		//}
+
 
     }
 	else{// Utente non trovato, mando un json con utente non trovato
--- logout.php
+++ logout.php
@@ -1,7 +1,16 @@
 <?php
-//avvia la Sessione
+//includo il db
 include("iot_park.php");
 mysqli_select_db($conn,$db_database);
+//leggi l'id
+$id_usr=$_POST["id_usr"];
+//$idName= "SELECT id_usr,logged FROM utente WHERE id_usr= '".$id_usr."'";
+$querySlog= "UPDATE utente SET logged='0' WHERE id_usr ='".$row["id_usr"]."'";
 
-exit();
+$data = array(
+  "id" => $row["id_usr"],
+  "logged" => $row["logged"]
+)
+$json=json_encode($data);
+ echo $json;
 ?>
