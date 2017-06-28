25d536c9e99f99ef1f829dd9dc25b1818a122b70
VRHero92 <luigipizzuto1992@gmail.com>
2017-06-28 12:44:53 +0200

    Ho cambiato il riferimento all'indice della tabella Parcheggio.

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
 create table utenti(
-id_par int(11) auto_increment primary key,
+id_usr int(11) auto_increment primary key,
 username varchar(30),
 password varchar(30),
 logged tinyint(1) unsigned zerofill not null default '0'
@@ -10,5 +10,5 @@ id_par int(11) auto_increment primary key,
 stato tinyint(1),
 utente int(11),
 INDEX new_utente(utente),
-FOREIGN KEY (utente) REFERENCES utenti(id)
+FOREIGN KEY (utente) REFERENCES utenti(id_usr)
 )engine="InnoDB";
