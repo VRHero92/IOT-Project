c02ac84f9c65227d40b87a7b729f386b854525a9
Xeena93 <alexa93k@gmail.com>
2017-06-28 11:35:44 +0200

    modifica register.php

---
+++ register.php
@@ -0,0 +1,31 @@
+<?php
+//avvia la Sessione
+session_start();
+include("iot_park.php");
+mysqli_select_db($conn,$db_database);
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+$password=md5($password);
+
+
+if(isset($_POST["username"]) && isset($_POST["password"])&& isset($_POST["ruolo"])){
+  $result = mysqli_query($conn, "INSERT INTO utente(username,password) VALUES(\"".$username."\", \"".$password."\")");
+        if($result){
+          $res=array("status"=>"OK", );
+          $json=json_encode($res);
+          echo $json;
+        }
+}
+else{
+  $res=array(
+    "status"=>"NOT",
+  );
+  $json=json_encode($res);
+  echo $json;
+}
+
+
+			}
+
+?>
