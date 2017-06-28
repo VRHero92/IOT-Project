6c798f17707523faadfbbea7bc2db51822419639
VRHero92 <luigipizzuto1992@gmail.com>
2017-06-28 14:03:45 +0200

    Ho inserito la funzione di controllo di Register.php

--- register.php
+++ register.php
@@ -10,22 +10,23 @@ $password=md5($password);
 
 
 if(isset($_POST["username"]) && isset($_POST["password"])){
+  $verifica = mysqli_query($conn, "select * from UTENTI where username = '".$username."'");
+  if(mysqli_num_rows($verifica)>0){
+    $res =array("status"=>"EXISTS", );
+  }else{
   $result = mysqli_query($conn, "INSERT INTO utente(username,password) VALUES(\"".$username."\", \"".$password."\")");
         if($result){
           $res=array("status"=>"OK", );
           $json=json_encode($res);
           echo $json;
-        }
-}
-else{
+        }else{
   $res=array(
     "status"=>"NOT",
   );
+  }
+  }
   $json=json_encode($res);
   echo $json;
 }
-
-
-			}
 
 ?>
