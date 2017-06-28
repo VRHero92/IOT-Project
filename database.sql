8b5c1fa2144a611e7ace1c85bb8bcdbd70214281
VRHero92 <luigipizzuto1992@gmail.com>
2017-06-28 10:44:52 +0200

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,14 @@
+create table utenti(
+id_par int(11) auto_increment primary key,
+username varchar(30),
+password varchar(30),
+logged tinyint(1) unsigned zerofill not null default '0'
+) engine = "InnoDB";
+
+create table parcheggio(
+id_par int(11) auto_increment primary key,
+stato tinyint(1),
+utente int(11),
+INDEX new_utente(utente),
+FOREIGN KEY (utente) REFERENCES utenti(id)
+)engine="InnoDB";
