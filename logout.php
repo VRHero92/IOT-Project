2a2ba9e5e3b4e0c0bfe1f05088cf6ac61bbc09a9
VRHero92 <luigipizzuto1992@gmail.com>
2017-06-28 16:15:20 +0200

    Sistemato logout.php. Dovrebbe funzionare ma non garantisco niente hahahaha

--- logout.php
+++ logout.php
@@ -4,11 +4,14 @@ include("iot_park.php");
 mysqli_select_db($conn,$db_database);
 //leggi l'id
 $id_usr=$_POST["id_usr"];
-//$idName= "SELECT id_usr,logged FROM utente WHERE id_usr= '".$id_usr."'";
-$querySlog= "UPDATE utente SET logged='0' WHERE id_usr ='".$row["id_usr"]."'";
+$idName= "SELECT id_usr,logged FROM utente WHERE id_usr= '".$id_usr."'";
+$querySlog= "UPDATE utente SET logged='0' WHERE id_usr ='".$id_usr."'";
+$log1 = mysqli_query($conn,$idName);
+$log2 = mysqli_query($conn,$querySlog);
+$row = mysqli_fetch_array($log1, MYSQLI_ASSOC);
 
 $data = array(
-  "id" => $row["id_usr"],
+  "id" => $id_usr,
   "logged" => $row["logged"]
 )
 $json=json_encode($data);
