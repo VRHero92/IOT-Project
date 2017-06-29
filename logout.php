c70c5e6fc73fe94e951f61a7a44354b5d80ebcc8
Xeena93 <alexa93k@gmail.com>
2017-06-29 09:13:14 +0200

    Modifiche su login e logout. aggiunta in entrambi del campo status. nel logout ho inserito il fetch in un if perchè dobbiamo SEMPRE controllare la validità del risultato della query

--- logout.php
+++ logout.php
@@ -4,16 +4,33 @@ include("iot_park.php");
 mysqli_select_db($conn,$db_database);
 //leggi l'id
 $id_usr=$_POST["id_usr"];
+//seleziono l'id utente e il suo stato che sarà logged=1
 $idName= "SELECT id_usr,logged FROM utente WHERE id_usr= '".$id_usr."'";
+//cambio logged in 0, si è sloggato
 $querySlog= "UPDATE utente SET logged='0' WHERE id_usr ='".$id_usr."'";
+//faccio entrambe le query
 $log1 = mysqli_query($conn,$idName);
 $log2 = mysqli_query($conn,$querySlog);
-$row = mysqli_fetch_array($log1, MYSQLI_ASSOC);
-
+//inserisco nelle row il risultato
+if($row = mysqli_fetch_array($log1, MYSQLI_ASSOC)){//è andata a buon fine
+//inserisco in un array
 $data = array(
   "id" => $id_usr,
-  "logged" => $row["logged"]
+  "logged" => $row["logged"],
+  "status"=>"OK"
 )
+//e impacchetto tutto in un json da mandare
 $json=json_encode($data);
  echo $json;
+ }
+ else{//non è andata a buon fine
+   $data = array(
+     "id" => "",
+     "logged" => "",
+     "status"=>"NOT"
+   )
+   //e impacchetto tutto in un json da mandare
+   $json=json_encode($data);
+    echo $json;
+ }
 ?>
