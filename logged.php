c031e2fcea62c402f46fd66f8795b2c8bae281df
VRHero92 <luigipizzuto1992@gmail.com>
2017-06-29 10:11:22 +0200

    Aggiunti commenti.

--- logged.php
+++ logged.php
@@ -2,21 +2,22 @@
 include("iot_park.php");
 mysqli_select_db($conn,$db_database);
 
-$id_usr=$_POST["id_usr"];
+$id_usr=$_POST["id_usr"]; //prendiamo il valore utente che ci passa Nunzio
 
+//Nella query, usiamo id_usr per recuperare lo stato logged dell'utente con quell'id.
 $ricercaLOG= "SELECT id_usr,logged FROM utente WHERE id_usr ='".$id_usr."'";
-$result = mysqli_query($conn, $ricercaLOG);
-if($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-  $data = array(
+$result = mysqli_query($conn, $ricercaLOG); //qui eseguiamo la query
+if($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){ //separiamo la tabella nelle righe.
+  $data = array( //chiudiamo i risultati in un array.
 		"id" => $row["id_usr"],
 		"logged" => $row["logged"],
 		"status"=>"OK"
 	)
-
+  //che ritorni 0 o 1, va qui.
 	$json=json_encode($data);
 	echo $json;
 
-}else{
+}else{ //se succede qualcosa di inaspettato, va qui.
 	$data = array(
 	"id" => "",
 	"logged" => "",
